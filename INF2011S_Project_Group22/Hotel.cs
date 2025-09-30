1ec642dfd9a7c3b421d35e41cb472b81d8cf2004
Aliyah Mongratie <MNGALI003@myuct.ac.za>
2025-09-30 07:28:07 +0200

    Added Hotel Class

---
+++ INF2011S_Project_Group22/Hotel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2011S_Project_Group22
+{
+    class Hotel
+    {
+        // aliyah's part 
+
+        #region Data Members 
+        private int hotelID;
+        private string hotelName;
+        private string hotelAddress;
+        private string hotelCity;
+        private string hotelPostCode;
+        private string hotelPhone;
+        private string hotelEmail;
+        private int noOfRooms;
+        #endregion
+
+        #region Property methods 
+
+        public int HotelID
+        {
+            get { return hotelID; }
+            set { hotelID = value; }
+        }
+
+        public string HotelName
+        {
+            get { return hotelName; }
+            set { hotelName = value; }
+        }
+
+        public string HotelAddress 
+        {
+            get { return hotelAddress; }
+            set { hotelAddress = value; }
+        }
+        public string HotelCity
+        {
+            get { return hotelCity; }
+            set { hotelCity = value; }
+        }
+        public string HotelPostCode
+        {
+            get { return hotelPostCode; }
+            set { hotelPostCode = value; }
+        }
+        public string HotelPhone
+        {
+            get { return hotelPhone; }
+            set { hotelPhone = value; }
+        }
+        public string HotelEmail
+        {
+            get { return hotelEmail; }
+            set { hotelEmail = value; }
+        }
+        public int NoOfRooms
+        {
+            get { return noOfRooms; }
+            set { noOfRooms = value; }
+        }
+        #endregion
+
+        #region Constructors 
+        public Hotel()
+        {
+            hotelID = 0;
+            hotelName = "";
+            hotelAddress = "";
+            hotelCity = "";
+            hotelPostCode = "";
+            hotelPhone = "";
+            hotelEmail = "";
+            noOfRooms = 0;
+        }
+        public Hotel(int newHotelID, string newHotelName, string newHotelAddress, string newHotelCity, string newHotelPostCode, string newHotelPhone, string newHotelEmail, int NewNoOfRooms)
+        {
+            hotelID = newHotelID;
+            hotelName = newHotelName;
+            hotelAddress = newHotelAddress;
+            hotelCity = newHotelCity;
+            hotelPostCode = newHotelPostCode;
+            hotelPhone = newHotelPhone;
+            hotelEmail = newHotelEmail;
+            noOfRooms = NewNoOfRooms;
+        }
+        #endregion
+
+
+    }
+}
