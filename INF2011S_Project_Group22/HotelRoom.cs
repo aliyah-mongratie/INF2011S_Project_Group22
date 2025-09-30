b762179614eba6ece919fb89d232579f609747c9
Aliyah Mongratie <MNGALI003@myuct.ac.za>
2025-09-30 08:40:12 +0200

    Added HotelRoom class

---
+++ INF2011S_Project_Group22/HotelRoom.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2011S_Project_Group22
+{
+    class HotelRoom
+    {
+        #region Data Members 
+        private int hotelRoomID;
+        private int hotelID;
+        public enum RoomStatus
+        {
+            Available = 0,
+            Occupied = 1
+        }
+        protected RoomStatus roomStat;
+        private decimal roomPrice;
+        private int roomCapacity;
+
+        #endregion
+
+        #region Property Methods 
+        public int HotelRoomID
+        {
+            get { return hotelRoomID; }
+            set { hotelRoomID = value; }
+        }
+        public int HotelID
+        {
+            get { return hotelID; }
+            set { hotelID = value; }
+        }
+        public RoomStatus getRoomStatus
+        {
+            get { return roomStat; }
+            set { roomStat = value; }
+        }
+        public decimal RoomPrice
+        {
+            get { return roomPrice; }
+            set { roomPrice = value; }
+        }
+        public int RoomCapacity
+        {
+            get { return roomCapacity; }
+            set { roomCapacity = value; }
+        }
+        #endregion
+
+        #region Constructors 
+        public HotelRoom()
+        {
+            hotelRoomID = 0;
+            hotelID = 0;
+            roomStat = HotelRoom.RoomStatus.Available;
+            roomPrice = 0;
+            roomCapacity = 0;
+        }
+        public HotelRoom(int newHotelRoomID, int newHotelID, decimal newRoomPrice, int newRoomCapacity)
+        {
+            hotelRoomID = newHotelRoomID;
+            hotelID = newHotelID;
+            roomStat = HotelRoom.RoomStatus.Available;
+            roomPrice = newRoomPrice;
+            roomCapacity = newRoomCapacity;
+        }
+
+        #endregion
+
+        #region Methods 
+
+        #endregion
+    }
+
+
+}
