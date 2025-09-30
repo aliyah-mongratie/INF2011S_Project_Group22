568da86f9628df6aa2928b123743e21a62e290b9
2005-tylor <tylorlafleur6@gmail.com>
2025-10-01 00:18:29 +0200

    Travel Agent class added

--- INF2011S_Project_Group22/Business/TravelAgent.cs
+++ INF2011S_Project_Group22/Business/TravelAgent.cs
@@ -8,11 +8,5 @@ namespace INF2011S_Project_Group22.Business
 {
     internal class TravelAgent : Person
     {
-        // Travel Agent inherits from Person
-        #region Data Members
-        public string TravelAgentId { get; set; }
-        public string AgencyName { get; set; }
-
-        #endregion
     }
 }
