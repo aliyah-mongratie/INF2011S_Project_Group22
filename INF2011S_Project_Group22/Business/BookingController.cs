97222eabf60189733da5dd7fac3873c326516bba
Aliyah Mongratie <MNGALI003@myuct.ac.za>
2025-10-01 11:07:29 +0200

    Make a booking Method added to BookingController

--- INF2011S_Project_Group22/Business/BookingController.cs
+++ INF2011S_Project_Group22/Business/BookingController.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace INF2011S_Project_Group22.Business
 {
@@ -25,11 +26,29 @@ namespace INF2011S_Project_Group22.Business
         #endregion
 
         #region Methoods 
-        /*public Booking MakeBooking(int bookResNumber, Guest guest, string bookingType,int numOfPeople, int numOfRooms, 
+       /* public Booking MakeBooking(int bookResNumber, Guest guest, string bookingType,int numOfPeople, int numOfRooms, 
                         DateTime checkInDate, DateTime checkOutDate, string specialRequirements)
         {
+            if(numOfPeople > 6)
+            {
+                MessageBox.Show("The number of people for a booking cannot exceed 6.", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+           
+            if(numOfRooms < 1 || numOfRooms >3)
+            {
+                MessageBox.Show("The number of rooms for a booking must be between 1 and 3.", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            // 4 people in a room? 
 
-        }*/
+            if(checkInDate >= checkOutDate)
+            {
+                MessageBox.Show("The check in date must be before the check out date", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            Booking booking = new Booking(bookResNumber, guest, bookingType, numOfPeople, numOfRooms, checkInDate, checkOutDate, specialRequirements);
+            bookings.Add(booking);
+            return booking;
+        } */
         #endregion
     }
 }
