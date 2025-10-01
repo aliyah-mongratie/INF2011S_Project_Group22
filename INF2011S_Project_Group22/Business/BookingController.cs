72dc6511da4f3d87ccec778e7f6686e25a10bc53
Aliyah Mongratie <MNGALI003@myuct.ac.za>
2025-10-01 10:04:55 +0200

    Booking Controller In progress

---
+++ INF2011S_Project_Group22/Business/BookingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2011S_Project_Group22.Business
+{
+    class BookingController //aliyah 
+    {
+        #region Data Members 
+        Collection<Booking> bookings;
+        #endregion
+
+        #region Property methods 
+        public Collection<Booking> AllBookings
+        {
+            get { return bookings; }
+        }
+        #endregion
+
+        #region Constructor
+        //added when DB classes are added 
+        #endregion
+
+        #region Methoods 
+        /*public Booking MakeBooking(int bookResNumber, Guest guest, string bookingType,int numOfPeople, int numOfRooms, 
+                        DateTime checkInDate, DateTime checkOutDate, string specialRequirements)
+        {
+
+        }*/
+        #endregion
+    }
+}
