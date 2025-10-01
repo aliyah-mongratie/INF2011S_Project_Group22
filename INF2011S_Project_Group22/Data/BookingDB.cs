6c9bbaa5188e8a972e8ed6bb5a21c8d99463aa3a
Aliyah Mongratie <MNGALI003@myuct.ac.za>
2025-10-02 00:57:30 +0200

    Added to BookingDB

--- INF2011S_Project_Group22/Data/BookingDB.cs
+++ INF2011S_Project_Group22/Data/BookingDB.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using INF2011S_Project_Group22.Business;
+using static INF2011S_Project_Group22.HotelRoom;
 
 namespace INF2011S_Project_Group22.Data
 {
@@ -17,7 +18,9 @@ namespace INF2011S_Project_Group22.Data
         private string sqlLocal1 = "SELECT * FROM Booking";
         private string table2 = "BookingRoom";
         private string sqlLocal2 = "SELECT * FROM BookingRoom";
-        
+        private string table3 = "HotelRoom";
+        private string sqlLocal3 = "SELECT * FROM BookingRoom";
+
         private Collection<Booking> bookings;
 
         #endregion
@@ -38,7 +41,9 @@ namespace INF2011S_Project_Group22.Data
             Add2Collection(table1);
             FillDataSet(sqlLocal2, table2);
             Add2Collection(table2);
-           
+            FillDataSet(sqlLocal3, table3);
+            Add2Collection(table3);
+
         }
         #endregion
 
@@ -69,10 +74,122 @@ namespace INF2011S_Project_Group22.Data
                     booking.checkOutDate = Convert.ToDateTime(myRow["CheckOutDate"]);
                     booking.specialRequirements = Convert.ToString(myRow["SpecialRequirements"]).TrimEnd();
 
+                    foreach (DataRow brRow in dsMain.Tables[table2].Select($"BookingId = {booking.bookingResNumber}"))
+                    {
+                        DataRow roomRow = dsMain.Tables[table3].Rows.Find(brRow["HotelRoomId"]);
+                        if (roomRow != null)
+                        {
+                            HotelRoom room = new HotelRoom();
+                            room.HotelRoomID = Convert.ToString(roomRow["HotelRoomId"]).TrimEnd();
+                            room.getRoomStatus = (HotelRoom.RoomStatus)Convert.ToByte(myRow["RoomStatus"]);
+                            room.RoomPrice = Convert.ToDecimal(roomRow["RoomPrice"]);
+                            room.RoomCapacity = Convert.ToInt32(roomRow["RoomCapacity"]);
+                            
+                            booking.Rooms.Add(room);
+                        }
+                    }
+                    bookings.Add(booking);
                 }
             }
         }
+        private void FillRow(DataRow aRow, Booking booking, DB.DBOperation operation)
+        {
+            aRow["BookingResNumber"] = booking.bookingResNumber;
+            aRow["GuestId"] = booking.guest.guestID;
+            aRow["TravelAgentId"] = booking.travelAgent.TravelAgentId;
+            aRow["BookingStatus"] = booking.bookingStat;
+            aRow["BookingType"] = booking.bookingType;
+            aRow["numOfPeople"] = booking.numOfPeople;
+            aRow["numOfRooms"] = booking.numOfRooms;
+            aRow["CheckInDate"] = booking.checkInDate;
+            aRow["CheckOutDate"] = booking.checkOutDate;
+            aRow["SpecialRequirements"] = booking.specialRequirements;
+
+        }
+        private void FillRowBookingRoom(DataRow aRow, HotelRoom room, int bookingResNumber)
+        {
+            aRow["BookingResNumber"] = bookingResNumber;
+            aRow["HotelRoomId"] = room.HotelRoomID;
+        }
+        private int FindRow(Booking booking, string table)
+        {
+            int rowIndex = 0;
+            DataRow myRow;
+            int returnValue = -1;
+
+            foreach (DataRow myRow_loopVariable in dsMain.Tables[table].Rows)
+            {
+                myRow = myRow_loopVariable;
+                if (myRow.RowState != DataRowState.Deleted)
+                {
+                    if (booking.bookingResNumber == Convert.ToInt32(dsMain.Tables[table].Rows[rowIndex]["BookingResNumber"]))
+                    {
+                        returnValue = rowIndex;
+                    }
+                }
+                rowIndex++;
+            }
+            return returnValue;
+        }
         #endregion
+
+        #region Database operations CRUD
+        public void DataSetChange(Booking booking, DB.DBOperation operation)
+        {
+            DataRow aRow = null;
+
+            switch (operation)
+            {
+                case DB.DBOperation.add:
+                    // Booking
+                    aRow = dsMain.Tables[table1].NewRow();
+                    FillRow(aRow, booking, DBOperation.add);
+                    dsMain.Tables[table1].Rows.Add(aRow);
+
+                    // Rooms
+                    foreach (var room in booking.Rooms)
+                    {
+                        DataRow brRow = dsMain.Tables[table2].NewRow();
+                        FillRowBookingRoom(brRow, room, booking.bookingResNumber);
+                        dsMain.Tables[table2].Rows.Add(brRow);
+                    }
+                    break;
+
+                case DB.DBOperation.edit:
+                    // Booking
+                    aRow = dsMain.Tables[table1].Rows.Find(booking.bookingResNumber);
+                    if (aRow != null)
+                        FillRow(aRow, booking, DBOperation.edit);
+
+                    // Rooms: delete old, add current
+                    foreach (DataRow old in dsMain.Tables[table2].Select($"BookingResNumber={booking.bookingResNumber}"))
+                        old.Delete();
+
+                    foreach (var room in booking.Rooms)
+                    {
+                        DataRow brRow = dsMain.Tables[table2].NewRow();
+                        FillRowBookingRoom(brRow, room, booking.bookingResNumber);
+                        dsMain.Tables[table2].Rows.Add(brRow);
+                    }
+                    break;
+
+                case DB.DBOperation.delete:
+                    // Booking
+                    aRow = dsMain.Tables[table1].Rows.Find(booking.bookingResNumber);
+                    if (aRow != null)
+                        aRow.Delete();
+
+                    // Rooms
+                    foreach (DataRow old in dsMain.Tables[table2].Select($"BookingResNumber={booking.bookingResNumber}"))
+                        old.Delete();
+                    break;
+            }
+        
+        }
     }
+    #endregion
+    #region Build Parameters, Create Commands & Update database
+
+    #endregion
 }
 
