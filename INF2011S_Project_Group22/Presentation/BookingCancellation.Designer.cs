4884a5a23d46d66c94315af6e3f8e948c5dba739
munta <msamsodien64@gmail.com>
2025-10-01 22:09:15 +0200

    BookingCancellation

---
+++ INF2011S_Project_Group22/Presentation/BookingCancellation.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace INF2011S_Project_Group22.Presentation
+{
+    partial class frmBookingCancellation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBookingCancellation));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblBookingCancelled = new System.Windows.Forms.Label();
+            this.lblBookingNumberRemoved = new System.Windows.Forms.Label();
+            this.btnReturn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1349, 710);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(559, 311);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(229, 219);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblBookingCancelled
+            // 
+            this.lblBookingCancelled.AutoSize = true;
+            this.lblBookingCancelled.BackColor = System.Drawing.Color.OldLace;
+            this.lblBookingCancelled.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookingCancelled.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblBookingCancelled.Location = new System.Drawing.Point(520, 261);
+            this.lblBookingCancelled.Name = "lblBookingCancelled";
+            this.lblBookingCancelled.Size = new System.Drawing.Size(313, 36);
+            this.lblBookingCancelled.TabIndex = 2;
+            this.lblBookingCancelled.Text = "Booking Cancelled!";
+            // 
+            // lblBookingNumberRemoved
+            // 
+            this.lblBookingNumberRemoved.AutoSize = true;
+            this.lblBookingNumberRemoved.BackColor = System.Drawing.Color.OldLace;
+            this.lblBookingNumberRemoved.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookingNumberRemoved.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblBookingNumberRemoved.Location = new System.Drawing.Point(442, 553);
+            this.lblBookingNumberRemoved.Name = "lblBookingNumberRemoved";
+            this.lblBookingNumberRemoved.Size = new System.Drawing.Size(485, 36);
+            this.lblBookingNumberRemoved.TabIndex = 3;
+            this.lblBookingNumberRemoved.Text = "Booking Number XXXXX Removed";
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnReturn.BackgroundImage")));
+            this.btnReturn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnReturn.Location = new System.Drawing.Point(12, 642);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(223, 52);
+            this.btnReturn.TabIndex = 4;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // frmBookingCancellation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 706);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.lblBookingNumberRemoved);
+            this.Controls.Add(this.lblBookingCancelled);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmBookingCancellation";
+            this.Text = "BookingCancellation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblBookingCancelled;
+        private System.Windows.Forms.Label lblBookingNumberRemoved;
+        private System.Windows.Forms.Button btnReturn;
+    }
+}
