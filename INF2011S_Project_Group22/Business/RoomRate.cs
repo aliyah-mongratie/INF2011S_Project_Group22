adb95b838de2d92d51234c581f0ca81ba34cbf59
moonie49 <imaancupido@gmail.com>
2025-10-01 18:20:45 +0200

    Created "RoomRate" class

---
+++ INF2011S_Project_Group22/Business/RoomRate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2011S_Project_Group22.Business
+{
+    internal class RoomRate
+    {
+        #region Data Members
+        public int roomRateID;
+        public string rate;
+
+        #endregion
+
+        #region Property Methods
+        
+        public int RoomRateID
+        {
+            get { return roomRateID; }
+            set { roomRateID = value; }
+        }
+        public string Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+        #endregion
+        #region Constructors
+
+        public RoomRate() // Default constructor
+        {
+            roomRateID = 0;
+            rate = "";
+        }
+        public RoomRate(int roomRateID, string rate) // Parameterized constructor
+        {
+            this.roomRateID = roomRateID;
+            this.rate = rate;
+        }
+        #endregion
+
+
+
+    }
+}
