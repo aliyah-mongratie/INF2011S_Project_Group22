c34260496de5ebe5712fb28f9019707612801fe8
aliyah-mongratie <mngali003@myuct.ac.za>
2025-10-06 18:11:32 +0200

    View Availability UI

---
+++ INF2011S_Project_Group22/Presentation/ViewAvailability.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace INF2011S_Project_Group22.Presentation
+{
+    partial class ViewAvailability
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewAvailability));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnResDetails = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lbl101 = new System.Windows.Forms.Label();
+            this.lbl102 = new System.Windows.Forms.Label();
+            this.lbl105 = new System.Windows.Forms.Label();
+            this.lbl104 = new System.Windows.Forms.Label();
+            this.lbl103 = new System.Windows.Forms.Label();
+            this.lblStatus101 = new System.Windows.Forms.Label();
+            this.lblStatus102 = new System.Windows.Forms.Label();
+            this.lblStatus103 = new System.Windows.Forms.Label();
+            this.lblStatus105 = new System.Windows.Forms.Label();
+            this.lblStatus104 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblUnavailable = new System.Windows.Forms.Label();
+            this.lblAvailable = new System.Windows.Forms.Label();
+            this.gbBackground = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.Control;
+            this.btnBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBack.BackgroundImage")));
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(601, 449);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(123, 48);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.TabStop = false;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // btnResDetails
+            // 
+            this.btnResDetails.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResDetails.BackgroundImage")));
+            this.btnResDetails.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResDetails.FlatAppearance.BorderSize = 0;
+            this.btnResDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResDetails.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResDetails.Location = new System.Drawing.Point(753, 437);
+            this.btnResDetails.Name = "btnResDetails";
+            this.btnResDetails.Size = new System.Drawing.Size(155, 60);
+            this.btnResDetails.TabIndex = 1;
+            this.btnResDetails.Text = "Enter Reservation Details";
+            this.btnResDetails.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(154, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 52);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(429, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 52);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(582, 312);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(69, 52);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(297, 312);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(69, 52);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(712, 200);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(69, 52);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lbl101
+            // 
+            this.lbl101.AutoSize = true;
+            this.lbl101.BackColor = System.Drawing.Color.OldLace;
+            this.lbl101.Location = new System.Drawing.Point(151, 264);
+            this.lbl101.Name = "lbl101";
+            this.lbl101.Size = new System.Drawing.Size(56, 13);
+            this.lbl101.TabIndex = 8;
+            this.lbl101.Text = "Room 101";
+            // 
+            // lbl102
+            // 
+            this.lbl102.AutoSize = true;
+            this.lbl102.BackColor = System.Drawing.Color.OldLace;
+            this.lbl102.Location = new System.Drawing.Point(426, 264);
+            this.lbl102.Name = "lbl102";
+            this.lbl102.Size = new System.Drawing.Size(56, 13);
+            this.lbl102.TabIndex = 9;
+            this.lbl102.Text = "Room 102";
+            // 
+            // lbl105
+            // 
+            this.lbl105.AutoSize = true;
+            this.lbl105.BackColor = System.Drawing.Color.OldLace;
+            this.lbl105.Location = new System.Drawing.Point(579, 381);
+            this.lbl105.Name = "lbl105";
+            this.lbl105.Size = new System.Drawing.Size(56, 13);
+            this.lbl105.TabIndex = 10;
+            this.lbl105.Text = "Room 105";
+            // 
+            // lbl104
+            // 
+            this.lbl104.AutoSize = true;
+            this.lbl104.BackColor = System.Drawing.Color.OldLace;
+            this.lbl104.Location = new System.Drawing.Point(294, 381);
+            this.lbl104.Name = "lbl104";
+            this.lbl104.Size = new System.Drawing.Size(56, 13);
+            this.lbl104.TabIndex = 11;
+            this.lbl104.Text = "Room 104";
+            // 
+            // lbl103
+            // 
+            this.lbl103.AutoSize = true;
+            this.lbl103.BackColor = System.Drawing.Color.OldLace;
+            this.lbl103.Location = new System.Drawing.Point(709, 264);
+            this.lbl103.Name = "lbl103";
+            this.lbl103.Size = new System.Drawing.Size(56, 13);
+            this.lbl103.TabIndex = 12;
+            this.lbl103.Text = "Room 103";
+            // 
+            // lblStatus101
+            // 
+            this.lblStatus101.AutoSize = true;
+            this.lblStatus101.BackColor = System.Drawing.Color.OldLace;
+            this.lblStatus101.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus101.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblStatus101.Location = new System.Drawing.Point(73, 187);
+            this.lblStatus101.Name = "lblStatus101";
+            this.lblStatus101.Size = new System.Drawing.Size(72, 108);
+            this.lblStatus101.TabIndex = 13;
+            this.lblStatus101.Text = ".";
+            // 
+            // lblStatus102
+            // 
+            this.lblStatus102.AutoSize = true;
+            this.lblStatus102.BackColor = System.Drawing.Color.OldLace;
+            this.lblStatus102.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus102.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblStatus102.Location = new System.Drawing.Point(348, 187);
+            this.lblStatus102.Name = "lblStatus102";
+            this.lblStatus102.Size = new System.Drawing.Size(72, 108);
+            this.lblStatus102.TabIndex = 14;
+            this.lblStatus102.Text = ".";
+            // 
+            // lblStatus103
+            // 
+            this.lblStatus103.AutoSize = true;
+            this.lblStatus103.BackColor = System.Drawing.Color.OldLace;
+            this.lblStatus103.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus103.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblStatus103.Location = new System.Drawing.Point(634, 190);
+            this.lblStatus103.Name = "lblStatus103";
+            this.lblStatus103.Size = new System.Drawing.Size(72, 108);
+            this.lblStatus103.TabIndex = 15;
+            this.lblStatus103.Text = ".";
+            // 
+            // lblStatus105
+            // 
+            this.lblStatus105.AutoSize = true;
+            this.lblStatus105.BackColor = System.Drawing.Color.OldLace;
+            this.lblStatus105.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus105.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblStatus105.Location = new System.Drawing.Point(501, 306);
+            this.lblStatus105.Name = "lblStatus105";
+            this.lblStatus105.Size = new System.Drawing.Size(72, 108);
+            this.lblStatus105.TabIndex = 16;
+            this.lblStatus105.Text = ".";
+            // 
+            // lblStatus104
+            // 
+            this.lblStatus104.AutoSize = true;
+            this.lblStatus104.BackColor = System.Drawing.Color.OldLace;
+            this.lblStatus104.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus104.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblStatus104.Location = new System.Drawing.Point(216, 304);
+            this.lblStatus104.Name = "lblStatus104";
+            this.lblStatus104.Size = new System.Drawing.Size(72, 108);
+            this.lblStatus104.TabIndex = 17;
+            this.lblStatus104.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.OldLace;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label5.Location = new System.Drawing.Point(787, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 108);
+            this.label5.TabIndex = 18;
+            this.label5.Text = ".";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.OldLace;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Crimson;
+            this.label6.Location = new System.Drawing.Point(787, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 108);
+            this.label6.TabIndex = 19;
+            this.label6.Text = ".";
+            // 
+            // lblUnavailable
+            // 
+            this.lblUnavailable.AutoSize = true;
+            this.lblUnavailable.BackColor = System.Drawing.Color.OldLace;
+            this.lblUnavailable.Location = new System.Drawing.Point(838, 363);
+            this.lblUnavailable.Name = "lblUnavailable";
+            this.lblUnavailable.Size = new System.Drawing.Size(63, 13);
+            this.lblUnavailable.TabIndex = 20;
+            this.lblUnavailable.Text = "Unavailable";
+            // 
+            // lblAvailable
+            // 
+            this.lblAvailable.AutoSize = true;
+            this.lblAvailable.BackColor = System.Drawing.Color.OldLace;
+            this.lblAvailable.Location = new System.Drawing.Point(838, 312);
+            this.lblAvailable.Name = "lblAvailable";
+            this.lblAvailable.Size = new System.Drawing.Size(50, 13);
+            this.lblAvailable.TabIndex = 21;
+            this.lblAvailable.Text = "Available";
+            // 
+            // gbBackground
+            // 
+            this.gbBackground.BackColor = System.Drawing.Color.OldLace;
+            this.gbBackground.Location = new System.Drawing.Point(23, 171);
+            this.gbBackground.Name = "gbBackground";
+            this.gbBackground.Size = new System.Drawing.Size(885, 260);
+            this.gbBackground.TabIndex = 22;
+            this.gbBackground.TabStop = false;
+            // 
+            // ViewAvailability
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(920, 509);
+            this.Controls.Add(this.lblAvailable);
+            this.Controls.Add(this.lblUnavailable);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblStatus104);
+            this.Controls.Add(this.lblStatus105);
+            this.Controls.Add(this.lblStatus103);
+            this.Controls.Add(this.lblStatus102);
+            this.Controls.Add(this.lblStatus101);
+            this.Controls.Add(this.lbl103);
+            this.Controls.Add(this.lbl104);
+            this.Controls.Add(this.lbl105);
+            this.Controls.Add(this.lbl102);
+            this.Controls.Add(this.lbl101);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnResDetails);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.gbBackground);
+            this.Name = "ViewAvailability";
+            this.Text = "ViewAvailability";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnResDetails;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lbl101;
+        private System.Windows.Forms.Label lbl102;
+        private System.Windows.Forms.Label lbl105;
+        private System.Windows.Forms.Label lbl104;
+        private System.Windows.Forms.Label lbl103;
+        private System.Windows.Forms.Label lblStatus101;
+        private System.Windows.Forms.Label lblStatus102;
+        private System.Windows.Forms.Label lblStatus103;
+        private System.Windows.Forms.Label lblStatus105;
+        private System.Windows.Forms.Label lblStatus104;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblUnavailable;
+        private System.Windows.Forms.Label lblAvailable;
+        private System.Windows.Forms.GroupBox gbBackground;
+    }
+}
