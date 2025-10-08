d15cceb0ae1e465ff83e355a60c58f3b636a33e2
aliyah-mongratie <mngali003@myuct.ac.za>
2025-10-08 11:10:01 +0200

    testing

--- INF2011S_Project_Group22/Data/DB.cs
+++ INF2011S_Project_Group22/Data/DB.cs
@@ -8,7 +8,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
+
+
 using INF2011S_Project_Group22.Properties;
+
+
+//testing commit
 
 namespace INF2011S_Project_Group22.Data
 {
@@ -16,7 +21,9 @@ namespace INF2011S_Project_Group22.Data
     {
         #region Variable declaration
 
-        private string strConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Imaan Cupido\\Desktop\\hopefullythisworks\\INF2011S_Project_Group22\\HotelBookingDB.mdf\";Integrated Security=True;Encrypt=False";//connection string used to connect to the HotelBookingDB
+       //
+       private string strConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\hello\\Desktop\\INF2011S_Group22\\INF2011S_Project_Group22\\HotelBookingDB.mdf;Integrated Security=True;Encrypt=False";//connection string used to connect to the HotelBookingDB
+        //private string strConn = Settings.Default.HotelBookingDBConnectionString; //connection string used to connect to the HotelBookingDB
         protected SqlConnection cnMain; //represents a connection in the database 
         protected DataSet dsMain; //An in-memory collection of the data
         protected SqlDataAdapter daMain; // delivers data from the database to the dataset
