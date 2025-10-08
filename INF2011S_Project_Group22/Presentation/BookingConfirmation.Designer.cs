5820a400f71454e02ea8c8689c4a27155db8f375
imaanC <imaancupido@gmail.com>
2025-10-08 13:53:50 +0200

    Revert "paymnt page"

--- INF2011S_Project_Group22/Presentation/BookingConfirmation.Designer.cs
+++ INF2011S_Project_Group22/Presentation/BookingConfirmation.Designer.cs
@@ -77,7 +77,6 @@
             this.btnBack.TabIndex = 5;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // BookingConfirmation
             // 
